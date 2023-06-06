6a9cca4017f4feebcff7bc9b0181662f988e0df1
Lolopold_ <110690874+Lolopold@users.noreply.github.com>
2023-06-06 18:38:01 +0200

    Update password gfg.py
    -changed .samples to .choices
    -ValueErrors eliminated

--- password gfg.py
+++ password gfg.py
@@ -1,8 +1,12 @@
 import random
 list_of_chr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+}{:"
-length_password = int(input("Length of password should be greater be greater than 6:		 "))
-random_password = "".join(random.sample(list_of_chr,k = length_password))
-if length_password > 6:
-	print(f"Your password is \n{random_password}")
-else:
-	print("password cannot be generated")
+try:
+	length_password = int(input("Length of password should be greater be greater than 6:		 "))
+	random_password = "".join(random.choices(list_of_chr, k = length_password))
+	if length_password > 6:
+		print("Your password is: \n"+(random_password))
+	else:
+		print("password cannot be generated")
+except ValueError:
+	pass
+	print("That wasn't a whole number")
